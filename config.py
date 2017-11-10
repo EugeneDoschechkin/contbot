1319b6b9113725a3126508f1fbb8b10c5e6529b8
Eugene <epsidani45@gmail.com>
2017-11-10 15:55:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 ﻿#!/usr/bin/env python3
 #-*- coding:utf-8 -*-
 
-token = '464380952:AAE3kTNtbN4rkbrDqIRQecxYgKpYNsK83Gw'
-ico_key = '79eee7a88a834b4ab4cf8d249476ac97'
-ico_sec = 'c42e9e82ed7c4ae5af2e769d77297d7b'
-pix_key = '6738814-d7c63421f0ce2c77da487bf08'
+token = 'TelegramAPItoken'
+ico_key = 'nounprojectKEY'
+ico_sec = 'nounprojectSECRET'
+pix_key = 'PixabayAPItoken'
 
 sorry = 'Sorry, no results \U0001F914'
 start = "Hi there! My name is Content Assistant! I want to help you to get some content for your project whatever that is.\n/photo -  Get yourself a free photo from Pixabay.com.\n/art -  Get yourself a free illustration from Pixabay.com.\n/vector -  Get yourself a free vector image from Pixabay.com.\n/icon - Get yourself a free icon from thenounproject.com.\n/music - Get yourself a free music from ccMixter.org. REMEMBER that you must give credit to the musicians.\n/gif - Get yourself a random gif from gifbin.com"
